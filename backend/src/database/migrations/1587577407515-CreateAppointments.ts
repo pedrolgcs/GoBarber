d0b299e821b74a261c3ab3353e2b0e1bbb183341
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-04-25 14:18:13 -0300

    create and list appointments

--- backend/src/database/migrations/1587577407515-CreateAppointments.ts
+++ backend/src/database/migrations/1587577407515-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587577407515
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,29 +1,31 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentRouter.get('/', (req, res) => {
-  const appointments = appointmentsRepository.all();
-
-  return res.status(200).json(appointments);
+appointmentRouter.get('/', async (req, res) => {
+  try {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
+    return res.status(200).json(appointments);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 });
 
-appointmentRouter.post('/', (req, res) => {
+appointmentRouter.post('/', async (req, res) => {
   try {
     const { provider, date } = req.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       provider,
       date: parsedDate,
     });
