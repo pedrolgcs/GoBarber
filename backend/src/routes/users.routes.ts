a3643636131b2bf0c2eb2ae5bdb097fc402eea37
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-04-26 16:13:02 -0300

    password hash for users

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 
+import User from '../models/User';
 import CreateUserService from '../services/CreateUserService';
 
 const userRouter = Router();
+
+userRouter.get('/', async (req, res) => {
+  try {
+    const usersRepository = getRepository(User);
+    const users = await usersRepository.find();
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
 
 userRouter.post('/', async (req, res) => {
   try {
@@ -11,6 +23,8 @@ userRouter.post('/', async (req, res) => {
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
 
     return res.status(201).json(user);
   } catch (err) {
--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 
 import User from '../models/User';
 
@@ -19,7 +20,13 @@ class CreateUserService {
       throw Error('Email adrress already used');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
     await usersRepository.save(user);
 
     return user;
