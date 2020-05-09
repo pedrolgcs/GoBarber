d0ba7f610061af8944beb1036d1f66047c31631a
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-05-09 14:48:08 -0300

    eslint and prettier config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello GoBarber</h1>
+    </>
+  );
+};
+
+export default App;
