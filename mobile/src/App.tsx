daf6f992aad08b8e64e83aee46a1ea2764283b65
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-05-15 15:37:07 -0300

    routes signIn and signUp

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,13 +1,19 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-// import { Container } from './styles';
+// routes
+import Routes from './routes';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello Rocket</Text>
-    </View>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
