7af1ecd0dd27d8357eaa6c8213a1ba4dea19857d
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-05-15 13:18:31 -0300

    eslint config

---
+++ mobile/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello Rocket</Text>
+    </View>
+  );
+};
+
+export default App;
