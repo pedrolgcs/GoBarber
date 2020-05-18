d3da5506f56f4d01e61aedca363fd0972abff2b7
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-05-18 11:18:51 -0300

    fix KeyboardAvoidingView

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigate = useNavigation();
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={20}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+          <Button>Entrar</Button>
+        </Container>
+
+        <BackToSignIn onPress={() => navigate.goBack()}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default SignUp;
