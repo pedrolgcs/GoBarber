93d3d21b3e8ba6caecdb2077bba26d2c217d4e3c
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-05-26 14:48:22 -0300

    jest coverage

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,26 @@
-test('sum two numbers ', () => {
-  expect(1 + 2).toBe(3);
+// Repository
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+// Services
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeappointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeappointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1');
+  });
+
+  /* it('should not be able to create a tow appointment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  }); */
 });
