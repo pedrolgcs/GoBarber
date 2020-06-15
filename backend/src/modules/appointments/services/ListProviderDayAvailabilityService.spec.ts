f9e72db36fa40710e273cc82bf1338ffb8cf95c2
Pedro Henrique <pedro.lg.cs@gmail.com>
2020-06-15 01:32:53 -0300

    list ability hours for provider in day

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -21,12 +21,16 @@ describe('ListProvidersMonthAvailability', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 20, 8, 0, 0),
+      date: new Date(2020, 5, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 5, 20, 10, 0, 0),
+      date: new Date(2020, 5, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 5, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -39,9 +43,12 @@ describe('ListProvidersMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 9, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
